b1d435a104319e1ef03dc5b4a7600c6b9b934346
Alex Botelho <alexbotelho1@hotmail.com>
2020-04-02 19:59:54 -0300

    Inicio
    ok

--- resources/album/src/app/app.module.ts
+++ resources/album/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 @NgModule({
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+      MatIconModule,
     AppRoutingModule
   ],
   providers: [],
