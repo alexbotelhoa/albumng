fff1f971063ccb64256c00b2146e5509ec969bf2
Alex Botelho <alexbotelho1@hotmail.com>
2020-04-02 19:48:10 -0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Album</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+</head>
+<body>
+    <app-root></app-root>
+
+    <script src="{{ asset('js/runtime-es2015.js') }}" type="module"></script>
+    <script src="{{ asset('js/runtime-es5.js') }}" nomodule defer></script>
+    <script src="{{ asset('js/polyfills-es5.js') }}" nomodule defer></script>
+    <script src="{{ asset('js/polyfills-es2015.js') }}" type="module"></script>
+    <script src="{{ asset('js/styles-es2015.js') }}" type="module"></script>
+    <script src="{{ asset('js/styles-es5.js') }}" nomodule defer></script>
+    <script src="{{ asset('js/vendor-es2015.js') }}" type="module"></script>
+    <script src="{{ asset('js/vendor-es5.js') }}" nomodule defer></script>
+    <script src="{{ asset('js/main-es2015.js') }}" type="module"></script>
+    <script src="{{ asset('js/main-es5.js') }}" nomodule defer></script>
+</body>
+</html>
